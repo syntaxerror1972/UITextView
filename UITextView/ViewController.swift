6f2522aae10664e4baee956a4269475632810c6d
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-13 01:01:59 +0530

    Initial Commit

---
+++ UITextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextView
+//
+//  Created by Shrawan Shinde on 13/01/17.
+//  Copyright © 2017 Shrawan Shinde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
